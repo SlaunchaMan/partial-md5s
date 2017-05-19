46e555925afb2bbf086e8edf57def16dc0854825
Jeff Kelley <SlaunchaMan@gmail.com>
2017-05-19 16:47:43 -0400

    Initial Commit

---
+++ partial-md5s/main.swift
@@ -0,0 +1,12 @@
+//
+//  main.swift
+//  partial-md5s
+//
+//  Created by Jeff Kelley on 5/19/17.
+//  Copyright © 2017 Jeff Kelley. All rights reserved.
+//
+
+import Foundation
+
+print("Hello, World!")
+
